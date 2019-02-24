12064aecaae296ba984c6a9be87f5f6afa6f5b56
Nramirez1216 <47047453+Nramirez1216@users.noreply.github.com>
2019-02-24 11:12:34 -0500

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -1,50 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour{
 
-    public float speed;
-    public Text countText;
-    public Text winText;
-    
     private Rigidbody2D rb2d;
-    private int count;
+    public float speed;
+    public float jumpForce;
 
+    // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        count = 0;
-        winText.text = "";
-        SetCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+            Application.Quit();
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
-        if (Input.GetKey("escape"))
-            Application.Quit();
-    }
 
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag ("PickUp"))
-        {
-            other.gameObject.SetActive (false);
-            count = count + 1;
-            SetCountText();
-        }
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+
+        rb2d.AddForce(movement * speed);
+       
     }
-    void SetCountText()
+    void OnCollisionStay2D(Collision2D collision)
     {
-        countText.text = "Count: " + count.ToString(); 
-        if (count >=10)
-        {
-            winText.text = "You win!"; 
+        if(collision.collider.tag == "Ground") {
+
+            if(Input.GetKey(KeyCode.UpArrow)) {
+
+                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+            }
         }
     }
 }
